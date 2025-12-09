ac571e112c95e362adc762d47c179cc58b5556f9
leapwithluvi <luviaprilyansyahgabriel@gmail.com>
2025-12-09 11:59:18 +0800

    feat: implement Lenis smooth scroll - add Lenis with optimal config - wrap all pages inside App component - remove Lenis CDN from index.html - improve scrolling performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+// PAGES
+import { HeroPages } from "./pages/HeroPages";
+import { AboutPages } from "./pages/AboutPages";
+import { MySkillPages } from "./pages/MySkillPages";
+import { ProjectPages } from "./pages/ProjectPages";
+import { ContactPages } from "./pages/ContactPages";
+import { Footer } from "./components/Footer";
+
 export const App = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 0,
+      smoothWheel: true,
+      easing: (t) => t * (2 - t),
+    });
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <HeroPages />
+      <AboutPages />
+      <MySkillPages />
+      <ProjectPages />
+      <ContactPages />
+      <Footer />
+    </>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-// PAGES
-import { HeroPages } from "./pages/HeroPages";
-import { AboutPages } from "./pages/AboutPages";
-import { MySkillPages } from "./pages/MySkillPages";
-import { ProjectPages } from "./pages/ProjectPages";
-import { ContactPages } from "./pages/ContactPages";
-import { Footer } from "./components/Footer";
+import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HeroPages />
-    <AboutPages />
-    <MySkillPages />
-    <ProjectPages />
-    <ContactPages />
-    <Footer />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
